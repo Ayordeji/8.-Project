a63960fd4eeb4b45ed46303cf25fc45792f08b53
Ayodeji <ayoeluyemi2002@gmail.com>
2022-06-17 16:33:57 +0100

    I'm Done

--- script/script.js
+++ script/script.js
@@ -17,3 +17,6 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
+
+
+
